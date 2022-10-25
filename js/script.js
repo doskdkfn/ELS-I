44ec95ca190057c3f0a633304db21cc399d7f02c
doskdkfn <dawn_1216@naver.com>
2022-10-25 17:21:15 +0900

    2022-10-25 No m-slideToggle

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 
 $(document).ready(function() {
+    /***************** pc menu *****************/
+
     $('#gnb > li').hover(function() {
         $('.submenu, .submenu-3', this).stop().slideDown();
     }, function() {
         $('.submenu, .submenu-3', this).stop().slideUp();
     });
+
+
+    /***************** mobile-open & close *****************/
+
+    $("#m-open").click(function() {
+        $("#m-menu").animate({right: "0%"}, 500);
+        $("#m-back").fadeIn(600);
+    })
+
+    $(".close").click(function() {
+        $("#m-menu").animate({right: "-100%"}, 500);
+        $("#m-back").fadeOut(600);
+    })
+
+    /***************** m-toggle btn *****************/
+
+    $('.m-gnb-2').click(function() {
+        
+    })
+
+    // $('.m-gnb-2').click(function() {
+    //     $('.m-submenu').slideToggle();
+    // });
+   
 });
 
 
