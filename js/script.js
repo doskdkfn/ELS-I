7c0c3c3f55344a60b6704e1b0be27b8d6a4800a7
doskdkfn <dawn_1216@naver.com>
2022-10-27 17:26:53 +0900

    2022-10-27 No pc slide

--- js/script.js
+++ js/script.js
@@ -9,7 +9,74 @@ $(document).ready(function() {
     });
 
 
-    /***************** mobile-open & close *****************/
+    /***************** pc - slide *****************/
+
+    function prev() {
+        if(curPos > 0) {
+            $('button').removeAttr('disabled')
+            $($('#slide')[curPos]).toggleClass('active')
+            $($('.a')[curPos]).toggleClass('active-step')
+            $('#slide').hide();
+            curPos -= 1;
+            $($('#slide')[curPos]).toggleClass('active')
+            $($('.a')[curPos]).toggleClass('active-step')
+            $('.a').fadeIn(800);
+        }
+        if(curPos == 0){
+            $(".prev")[0].setAttribute("disabled",'true')
+        }
+    }
+
+    function next(){
+        if(curPos < 3){
+            $("button").removeAttr("disabled")
+            $($("#slide")[curPos]).toggleClass("active")
+            $($(".a")[curPos]).toggleClass("active-step")
+            $("#slide").hide();
+            curPos += 1;
+            $($("#slide")[curPos]).toggleClass("active")
+            $($(".a")[curPos]).toggleClass("active-step")
+            $(".a").fadeIn(800); 
+        }
+        if(curPos == 3){
+            $(".next")[0].setAttribute("disabled",'true')
+        }
+    }
+
+    function init(){
+        $("#slide").hide()
+        $(".a").show()
+        $(".left").click(prev)
+        $(".rigt").click(next)
+    }
+
+    function next() {
+        slider.stop().animate({
+            'left': -moveSlider_width * incre
+        }, 500);
+        numSlider.stop().animate({
+            'left': -numSlider_width * incre + num_init_left
+        }, 500);
+    }
+
+    function prev() {
+        slider.stop().animate({
+            'left': -moveSlider_width * incre
+        }, 500);
+        numSlider.stop().animate({
+            'left': -numSlider_width * incre + num_init_left
+        }, 500);
+    }
+
+    // var idx = 0;
+    // setInterval(function() {
+    //     $('#slide > .a').eq(idx).animate({left:'-100%'}, 400).animate({left:'-100%'}, 0);
+    //     idx==2?idx=0:idx++;
+    //     $('#slide > .a').eq(idx).animate({left:0}, 400);
+    // }, 3000);
+
+
+    /************* mobile-open & close ************/
 
     $("#m-open").click(function() {
         $("#m-menu").animate({right: "0%"}, 500);
